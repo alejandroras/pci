ac4f97700dddc0ebd729f7b7f72abfdb4f88c742
alejandroras <a01713901@tec.mx>
2024-09-08 22:44:28 -0600

    Add files via upload

--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -11,6 +11,8 @@
 # una opción de jugar nuevamente que mantenga un historial de las partidas y resultados pasados
 # implementar un sistema de puntuación basado en la cantidad de intentos y dificultad seleccionada en el juego
 import random
+import unidecode
+
 
 def configuración_juego():
     print("Hola! ¿Cuál es tu nombre?: ")
@@ -18,9 +20,9 @@ def configuración_juego():
 
     while True:
         
-        dificultad = input("Selecciona el nivel de dificultad (fácil, intermedio, difícil, personalizado): ").lower()
+        dificultad = unidecode.unidecode(input("Selecciona el nivel de dificultad (fácil, intermedio, difícil, personalizado): ").lower())
         
-        if dificultad == "fácil":
+        if dificultad == "facil":
             numero_min = 1
             numero_max = 10
             intentos_permitidos = 6
@@ -32,7 +34,7 @@ def configuración_juego():
             intentos_permitidos = 5
             break
         
-        elif dificultad == "difícil":
+        elif dificultad == "dificil":
             numero_min = 1
             numero_max = 50
             intentos_permitidos = 4
@@ -41,20 +43,22 @@ def configuración_juego():
         elif dificultad == "personalizado":
             numero_min = int(input("Ingresa el número mínimo del rango a adivinar: "))
             numero_max = int(input("Ingresa el número máximo del rango a adivinar: "))
-            intentos_permitidos = int(input("Ingresa el número de intenots permitidos: "))
+            intentos_permitidos = int(input("Ingresa el número de intentos permitidos: "))
             break
         
         else:
-            print("Selección inválida. Elige entre fácil, medio ,difícil o personalizado")
+            print("Selección inválida. Elige entre fácil, intermedio ,difícil o personalizado")
     
     return username, numero_min, numero_max, intentos_permitidos
 
-def obtener_adivinanza(intentos_ya_realizados):
+def obtener_adivinanza(intentos_ya_realizados, numero_min, numero_max):
     while True:
         try:
             adivina = int(input("¿En qué número estoy pensando?: "))
             if adivina in intentos_ya_realizados:
-                print("Ya habías intentado ese número. Prueba con otro.")
+                print("Ya habías intentado con ese número. Prueba con otro.")
+            elif adivina not in range(numero_min, numero_max + 1):
+                print(f"Por favor, ingresa un número entre el {numero_min} y el {numero_max}.")
             else:
                 return adivina
         except ValueError:
@@ -73,8 +77,7 @@ def checar_adivinanza(adivina, numero_pensado):
 
 def jugar_de_nuevo():
     respuesta = input("¿Te gustaría volver a jugar? (Sí/No): ").lower()
-    return respuesta == "sí"
-
+    return respuesta == "sí" or respuesta == "si"
 
 print("Bienvenido al juego de Adivina el Número!")
 
@@ -87,10 +90,12 @@ while True:
     print(f"Bueno, {username}, estoy pensando en un número entre el {numero_min} y el {numero_max}.")
 
     while intentos < intentos_permitidos:
-        adivina = obtener_adivinanza(intentos_previos)
-        intentos += 1
-        intentos_previos.add(adivina)
-
+        adivina = obtener_adivinanza(intentos_previos, numero_min, numero_max)
+        
+        if adivina not in intentos_previos:
+            intentos += 1
+            intentos_previos.add(adivina)
+        
         if checar_adivinanza(adivina, numero_pensado):
             print(f"\nAdivinaste el número en {intentos} intentos.")
             break
